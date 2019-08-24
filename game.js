d41a97cbc2298afe62559b4687b551b39dd91484
Darek <my_email@example.com>
2019-08-24 12:14:19 +0200

    steep 7

--- game.js
+++ game.js
@@ -2,8 +2,10 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var startGame = 0;
+var level = 0;
 
-var randomChosenColor = buttonColors[newsequence()];
+var randomChosenColor = buttonColors[newSequence()];
 gamePattern.push(randomChosenColor);
 
 $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -15,7 +17,16 @@ $( ".btn" ).click(function(e) {
   var userChosenColour = e.target.id;
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
-  console.log(userClickedPattern);
+  // console.log(e.target.classList[1]);
+  animatePress(e.target.classList[1]);
+
+});
+
+$("body").keypress(function() {
+  (startGame === 0) ? newSequence():0;  //0 - nic nie robi
+  (level === 0) ? $("h1").html("Level "+level):0;
+  // console.log( startGame);
+  startGame++;
 });
 
 
@@ -26,11 +37,19 @@ function playSound(key) {
 }
 
 
-function newsequence () {
+function newSequence () {
   var randomNumber = Math.round((Math.random() * 3));
   playSound(buttonColors[randomNumber]);
+  level++;
+  $("h1").html("Level "+level);
   return randomNumber;
 }
 
+function animatePress(currentColor) {
+  $( "."+currentColor ).addClass( currentColor +" pressed" )
+  setTimeout(function(){
+    $( "."+currentColor).removeClass("pressed");
+  }, 100);
+}
 
 
